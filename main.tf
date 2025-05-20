719fe8e61f942f1fb87945f4a832c22b817d3f58
Raja <127074887+RajaGattem@users.noreply.github.com>
2025-05-20 20:40:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "main_subnet" {
 }
 
 resource "aws_instance" "my-instance" {
-  ami           = "ami-0a7620d611d3ceae4" # Replace with a valid AMI in us-east-1
+  ami           = "ami-09b9b5665040249ad" # Replace with a valid AMI in us-east-1
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.main_subnet.id
 }
